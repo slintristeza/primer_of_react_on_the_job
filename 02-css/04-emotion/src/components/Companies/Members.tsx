5fa890dcf7183d8beab9da9e139e5cccf23788cc
slintristeza <slintristeza@gmail.com>
2019-07-29 23:06:29 +0900

    feat:02-04 src/components/Comapnies/Mmebers

---
+++ 02-css/04-emotion/src/components/Companies/Members.tsx
@@ -0,0 +1,61 @@
+/** @jsx jsx */
+import React, { FC } from 'react';
+import { css, jsx } from '@emotion/core';
+import { Card, Header, Image } from 'semantic-ui-react';
+import capitalize from 'lodash/capitalize';
+import { sprintf } from 'sprintf-js';
+
+import pages from '../../pages';
+import { User } from '../../services/github/models';
+import HtmlTitle from '../common/HtmlTitle';
+import Spinner from '../common/Spinner';
+
+const members = css`
+  margin: 2em 1em;
+`;
+const membersHeader = css`
+  margin-bottom: 1.25em !important;
+  text-align: center;
+`;
+
+export interface CompanyMembersProps {
+  companyName: string;
+  users: User[];
+  isLoading?: boolean;
+}
+
+const CompanyMembers: FC<CompanyMembersProps> = ({
+  companyName = '<会社名>',
+  users = [],
+  isLoading = false,
+}) => {
+  const title = sprintf(pages.companies.members.title, capitalize(companyName));
+
+  return (
+    <div>
+      <HtmlTitle title={title} />
+      <div css={members}>
+        <Header as="h2" css={membersHeader}>
+          {title}
+        </Header>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <Card.Group>
+            {users.map(user => (
+              <Card key={user.id} href={user.htmlUrl} target="_blank">
+                <Card.Content>
+                  <Image floated="right" size="mini" src={user.avatarUrl} />
+                  <Card.Header>{user.login}</Card.Header>
+                  <Card.Meta>GitHub ID: {user.id}</Card.Meta>
+                </Card.Content>
+              </Card>
+            ))}
+          </Card.Group>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CompanyMembers;
