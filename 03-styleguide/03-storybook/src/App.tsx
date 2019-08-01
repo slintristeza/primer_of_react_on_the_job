5be7274d25f22826296a5df872f01dde380de152
slintristeza <slintristeza@gmail.com>
2019-08-02 00:20:45 +0900

    feat:03-03 src/App

--- 03-styleguide/03-storybook/src/App.tsx
+++ 03-styleguide/03-storybook/src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+import React, { FC } from 'react';
+import { css, jsx } from '@emotion/core';
+import { Helmet } from 'react-helmet';
+import { Redirect, Route, Switch } from 'react-router';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Home from './components/Home';
+
+const appHeader = css`
+  background-color: #222;
+  color: white;
+  margin-bottom: 4em;
+  padding: 20px;
+  text-align: center;
+`;
+const title = 'GitHub API デモアプリ';
+
+const App: FC = () => (
+  <div>
+    <Helmet htmlAttributes={{ lang: 'ja' }}>
+      <title>{title}</title>
+    </Helmet>
+
+    <header css={appHeader}>
+      <h1>{title}</h1>
+    </header>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Redirect to="/" />
+    </Switch>
+  </div>
+);
 
 export default App;
