e40455ec7f7d57dc743b50259414d1e16270edfc
slintristeza <slintristeza@gmail.com>
2019-09-23 18:01:17 +0900

    feat:04-03 github.spec saga repositories

--- 04-unit-test/03-saga-test/src/sagas/github.spec.ts
+++ 04-unit-test/03-saga-test/src/sagas/github.spec.ts
@@ -3,9 +3,10 @@ import { AxiosError } from 'axios';
 import camelcaseKeys from 'camelcase-keys';
 
 import reducer, { initialState } from '../reducer';
-import { watchGetMembers } from './github';
+import { watchGetMembers, watchSearchRepositories } from './github';
 import * as actions from '../actions/github';
 import userData from '../services/github/__mocks__/users.json';
+import repoData from '../services/github/__mocks__/repositories.json';
 
 describe("GitHub API sagas' saga", () => {
   describe("Get members' saga", () => {
@@ -39,6 +40,42 @@ describe("GitHub API sagas' saga", () => {
         .withReducer(reducer as any)
         .put(actions.getMembers.fail(params, error as AxiosError))
         .dispatch(actions.getMembers.start(params))
+        .hasFinalState({ ...initialState, error })
+        .silentRun(1);
+    });
+  });
+
+  describe('Search repositories saga', () => {
+    const handler = jest.fn();
+    const stars = 'stars';
+    const params = { q: 'redux', sort: stars as typeof stars };
+    const repositories = camelcaseKeys(repoData, { deep: true });
+
+    it('should succeeded', async () => {
+      handler.mockReturnValue(repositories);
+
+      return expectSaga(watchSearchRepositories, handler)
+        .withReducer(reducer as any)
+        .put(actions.searchRepositories.succeed(params, { repositories }))
+        .dispatch(actions.searchRepositories.start(params))
+        .hasFinalState({ ...initialState, repositories })
+        .silentRun(1);
+    });
+
+    it('should failed with server error', async () => {
+      const error = {
+        message: 'Intrernal Server Error',
+        response: {
+          status: 500,
+          statusText: 'Intrernal Server Error',
+        },
+      };
+      handler.mockRejectedValue(error);
+
+      return expectSaga(watchSearchRepositories, handler)
+        .withReducer(reducer as any)
+        .put(actions.searchRepositories.fail(params, error as AxiosError))
+        .dispatch(actions.searchRepositories.start(params))
         .hasFinalState({ ...initialState, error })
         .silentRun(1);
     });
